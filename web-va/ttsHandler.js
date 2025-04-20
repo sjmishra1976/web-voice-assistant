0feb0f3478fc94a040d0c71aa6a654624e09790c
Santoshkumar Mishra <sjmishra1976@gmail.com>
2025-04-19 21:38:49 -0500

    ttsupdate

---
+++ web-va/ttsHandler.js
@@ -0,0 +1,18 @@
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+async function synthesizeSpeech(text) {
+  return new Promise((resolve, reject) => {
+    const filename = `public/tts/${uuidv4()}.wav`;
+
+    const command = `tts --text "${text}" --out_path ${filename}`;
+    exec(command, (err) => {
+      if (err) return reject(err);
+      resolve(filename);
+    });
+  });
+}
+
+module.exports = { synthesizeSpeech };
